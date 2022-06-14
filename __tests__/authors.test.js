be050adf38ca270bb627ce99f678aef3cdba9e7e
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-14 12:13:37 -0700

    added author test, passed with mock data

---
+++ __tests__/authors.test.js
@@ -0,0 +1,21 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('authors routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('should return a list of authors', async () => {
+    const res = await request(app).get('/authors');
+    expect(res.body.length).toEqual(2);
+    const phil = res.body.find((char) => char.id === '2');
+    expect(phil).toHaveProperty('first_name', 'Phil');
+    expect(phil).toHaveProperty('last_name', 'Hartman');
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
