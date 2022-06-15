055bea24b2810ba820d258734a0124c19714dfd5
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-14 21:48:54 -0700

    fixed works test

--- __tests__/works.test.js
+++ __tests__/works.test.js
@@ -2,17 +2,27 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { Work } = require('../lib/models/Work');
 
 describe('works routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it.skip('should add a new work', async () => {
-    const res = await request(app).get('/works');
-    expect(res.body.length).toEqual(1);
-    const work = res.body.find((char) => char.id === '1');
-    expect(work).toHaveProperty('author_id', '1');
-    expect(work).toHaveProperty('book_id', '1');
+  it('should add a new work', async () => {
+    const works = new Work({
+      author_id: '5',
+      book_id: '3',
+      title: 'ONE DAY I WILL BE GREAT',
+    });
+    const res = await request(app).post('/works').send(works);
+    expect(res.body.author_id).toEqual(works.author_id);
+    expect(res.body.book_id).toEqual(works.book_id);
+    expect(res.body.title).toEqual(works.title);
+    const count = await Work.count();
+    expect(count).toEqual(7);
+  });
+  afterAll(() => {
+    pool.end();
   });
 });
