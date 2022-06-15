2904df9d14bf0523fdeae5f5108df017e0e80344
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-14 19:38:48 -0700

    added id test to books.

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -14,6 +14,25 @@ describe('books routes', () => {
     const book = res.body.find((char) => char.id === '1');
     expect(book).toHaveProperty('title', 'A TIME TO KILL');
   });
+
+  it('should show author with book', async () => {
+    const res = await request(app).get('/books/5');
+    const book = {
+      id: '4',
+      title: 'PALE FIRE',
+      published: 1962,
+      authors: [
+        {
+          id: 4,
+          dob: 1899,
+          pob: 'Saint Petersburg',
+          last_name: 'Nabokov',
+          first_name: 'Vladimir',
+        },
+      ],
+    };
+    expect(res.body).toEqual(book);
+  });
   afterAll(() => {
     pool.end();
   });
