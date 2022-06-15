442f2d2540003299fcba8afecfeae749464a6e1c
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-14 19:21:06 -0700

    added id test for author

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -8,14 +8,31 @@ describe('authors routes', () => {
     return setup(pool);
   });
 
-  it('should return a list of authors', async () => {
+  it('should show author name', async () => {
     const res = await request(app).get('/authors');
     expect(res.body.length).toEqual(5);
     const john = res.body.find((char) => char.id === '1');
     expect(john).toHaveProperty('first_name', 'John');
     expect(john).toHaveProperty('last_name', 'Grisham');
-    expect(john).toHaveProperty('dob', 1955);
-    expect(john).toHaveProperty('pob', 'Arkansas US');
+  });
+
+  it('should show author with book', async () => {
+    const res = await request(app).get('/authors/1');
+    const john = {
+      id: '1',
+      first_name: 'John',
+      last_name: 'Grisham',
+      dob: 1955,
+      pob: 'Arkansas US',
+      books: [
+        {
+          id: 1,
+          title: 'A TIME TO KILL',
+          published: 1989,
+        },
+      ],
+    };
+    expect(res.body).toEqual(john);
   });
   afterAll(() => {
     pool.end();
